9f95f34e2231c27e436634ae35f7e248ca2f3666
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2021-08-01 20:35:27 -0300

    commit inicial

---
+++ src/main/java/org/exemplo/aulasjpa/AulasJpaApplication.java
@@ -0,0 +1,13 @@
+package org.exemplo.aulasjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/exemplo/aulasjpa/AulasJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exemplo.aulasjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
